e6088adb4135cb5d75727286215c20b310cd82d1
Hector Rosales <hector301rosales@gmail.com>
2021-09-30 13:24:33 -0500

    getting data from post request

--- myApp.js
+++ myApp.js
@@ -58,4 +58,10 @@ app.get("/name", (req, res) => {
     })
 })
 
+app.post("/name", (req, res) => {
+    res.json({
+        "name": req.body.first + " " + req.body.last
+    })
+});
+
 module.exports = app;
